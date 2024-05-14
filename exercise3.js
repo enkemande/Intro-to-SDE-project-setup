4f6a2965a3ae4f90334911b22c666a1373f0ffc6
Nash Nassah <nassah@Nashs-MacBook-Pro.local>
2024-05-13 20:12:26 -0500

    numbers.js

--- exercise3.js
+++ exercise3.js
@@ -1,9 +1,9 @@
 // Add your code here
 
 const myName = 'Chris';
-let myAge = '42';
+const myAge = 42;
 
-let myNewAge = "In 20years I will be {myAge + 20)";
+let myNewAge = `In 20 years I will be ${myAge + 20}`;
 
 console.log(myName);
 console.log(myNewAge);
---
+++ numbers.js
@@ -0,0 +1,17 @@
+let myInt = -2;
+let myFloat = 2.33;
+const evenNumber = 1
+
+let myDecimal = 2.333378495585858658;
+
+console.log(myDecimal.toFixed(2));
+console.log(Math.cos(evenNumber))
+
+const calculateCos = (value) => {
+    return 0.5* (3 + 2)
+}
+
+const mathResult = ((5 * 5) / 4 + ( 7 - 2)) + 6
+
+console.log(mathResult)
+console.log(calculateCos())
