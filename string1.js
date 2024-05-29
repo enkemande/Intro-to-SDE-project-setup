ffaf8f048c3432c353c0f0211f4901698be83a46
Nash Nassah <nassah@Nashs-MacBook-Pro.local>
2024-05-28 23:38:32 -0500

    string1.js

---
+++ string1.js
@@ -0,0 +1,8 @@
+const quoteStart = 'Don\'t judge each day by the harvest you reap';
+const quoteEnd = 'Wait till the end to determine your harvest';
+
+//concatenating the two strings
+const finalQuote = `${quoteStart}, ${quoteEnd}`;
+
+console.log(finalQuote); 
+
