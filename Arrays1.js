6ba0c73285c52a6a7293ca67b6ec2992a16dcbae
Nash Nassah <nassah@Nashs-MacBook-Pro.local>
2024-06-04 20:42:07 -0500

    Arrays1.js

---
+++ Arrays1.js
@@ -0,0 +1,10 @@
+const myArray = ["Beans", "Rice", "Vegetables"]; // Array with 3 items stored in variable
+myArray[0] = "Eggs"; // Modifying first item of array using bracket & assignment
+myArray[1] = "potatoes"; // Modifying second item of array using bracket & assignment
+
+console.log(myArray);
+
+// Adding a new item to the beginning of the array
+myArray.unshift("Yams");
+
+console.log(myArray);
