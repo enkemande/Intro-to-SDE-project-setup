11b3360c7ab71627a2c037f7a8cb318d6be15ee8
Nash Nassah <nassah@Nashs-MacBook-Pro.local>
2024-06-06 05:47:16 -0500

    Arrays4.js

---
+++ Arrays4.js
@@ -0,0 +1,31 @@
+const birds = [ "Parrots", "Falcons", "Eagles", "Emus", "Caracaras", "Egrets" ];
+
+1 // Finding the index of the Eagles item and hence using to remove the Eagles item
+const index = birds.indexOf("Eagles");
+
+console.log(index);
+
+1 // Removing Eagles item using splice() method
+if (index !== -1) {
+    birds.splice(index, 1);
+}
+
+console.log(birds);
+
+// making a new array called eBirds starting with letter "E"
+let eBirds = birds.toString();
+
+console.log(eBirds.includes ("E"));
+
+
+
+
+
+
+
+
+
+
+
+
+
