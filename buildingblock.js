94d93d42f67560411562e55c6aa9f1e7e04592c7
Nash Nassah <nassah@Nashs-MBP.attlocal.net>
2024-06-21 06:53:02 -0500

    buildingblock.js

---
+++ buildingblock.js
@@ -0,0 +1,35 @@
+// // CONDITIONALS - if else... statement syntax
+// if (condition) {
+//     //code to run if condition true
+// } else {
+//     //run some other code
+// }
+
+let shoppingDone = false;
+let childsAllowance;
+
+if (shoppingDone === true) {
+  childsAllowance = 10;
+} else {
+  childsAllowance = 5;
+}
+
+console.log(shoppingDone);
+
+// CONDITIONALS - else if... statement syntax
+   // Used when we have more than two conditions
+
+   let passageWay;
+
+   if ("Number of persons present" >= 2 ) {
+    passageWay = "Open";
+   } else if ("Number of persons present" <= 3) {
+    passageWay = "Open";
+   } else {
+    passageWay = "Closed";
+   }
+
+   console.log(passageWay);
+
+
+
