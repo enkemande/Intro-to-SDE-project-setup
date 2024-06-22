5891dad80fff49c96bcb85d8a6f628e8c101aecd
Nash Nassah <nassah@Nashs-MBP.attlocal.net>
2024-06-22 18:22:56 -0500

    conditionals1.js

---
+++ conditionals1.js
@@ -0,0 +1,23 @@
+let season = 'summer';
+let response;
+
+if (season === 'summer') {
+    response = 'We are in summer, welcome to summer!';
+    console.log(response);
+}
+else if (season === 'winter') {
+    response = 'We don\'t know what season it is';
+    console.log(response);
+}
+else {
+    response = 'We don\'t know what season it is';
+    console.log(response);
+}
+
+
+
+
+
+
+
+
