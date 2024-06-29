fc003b3eb2a8b5f86447ed6bd7a22de6d5c03ecd
Nash Nassah <nassah@Nashs-MBP.attlocal.net>
2024-06-29 01:03:00 -0500

    conditionalsassignment3.js

---
+++ conditionalsassignment3.js
@@ -0,0 +1,17 @@
+let machineActive = true;
+let pwd = 'cheese';
+
+let machineResult;
+let pwdResult;
+
+if (machineActive === true) {
+    machineResult = 'Machine switched ON';
+    console.log(machineResult);
+pwd === 'cheese' ? pwdResult = 'Password successful' : pwdResult ='Password Unsuccessful';
+console.log(pwdResult)
+} else {
+    machineResult = 'Machine switched OFF';
+    console.log(machineResult);
+}
+
+
