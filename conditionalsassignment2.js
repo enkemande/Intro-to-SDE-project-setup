0857a94ef915c8324a7c126c2f87f3b816e250ea
Nash Nassah <nassah@Nashs-MBP.attlocal.net>
2024-07-02 22:12:29 -0500

    Did the necessary corrections with the ranges

--- conditionalsassignment2.js
+++ conditionalsassignment2.js
@@ -8,26 +8,26 @@ if (machineActive === true) {
 if (score < 0 || score > 100) {
     response = 'This is not possible, an error has occurred.';
     console.log(response);
-} else if (0 <= score <= 19 ) {
+} else if (score >= 0 && score <= 19) {
     response = 'That was a terrible score — total fail!';
     console.log(response);
-} else if (20 <= score <= 39) {
+} else if (score >= 20 && score <= 39) {
     response = 'You know some things, but it\'s a pretty bad score. Needs improvement.';
     console.log(response);
-} else if (40 <= score <= 69) {
+} else if (score >= 40 &&  score <= 69) {
     response = 'You did a passable job, not bad!';
     console.log(response);
-} else if (70 <= score <= 89) {
+} else if (score >= 70 &&  score <= 89) {
     response = 'That\'s a great score, you really know your stuff.';
     console.log(response);
-} else if (90 <= score <= 100) {
+} else if (score >= 90 && score <= 100) {
     response = 'What an amazing score! Did you cheat? Are you for real?"';
     console.log(response);
 } else {
     response = 'Machine switched off, turn on machine';
     console.log(response);
 }
-}
+} 
 
 
 
