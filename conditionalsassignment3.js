209cbc7b7faf88bb78ca7a1f6f31a4c9c28aebe2
Nash Nassah <nassah@Nashs-MBP.attlocal.net>
2024-07-02 22:24:01 -0500

    Made few adjustments with the tenary operator

--- conditionalsassignment3.js
+++ conditionalsassignment3.js
@@ -7,7 +7,8 @@ let pwdResult;
 if (machineActive === true) {
     machineResult = 'Machine switched ON';
     console.log(machineResult);
-pwd === 'cheese' ? pwdResult = 'Password successful' : pwdResult ='Password Unsuccessful';
+// pwd === 'cheese' ? pwdResult = 'Password successful' : pwdResult ='Password Unsuccessful'; clean code correction below
+pwdResult = pwd === 'cheese' ? 'Password successful' : 'Password Unsuccessful';
 console.log(pwdResult)
 } else {
     machineResult = 'Machine switched OFF';
